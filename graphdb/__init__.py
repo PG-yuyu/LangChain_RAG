12316f5a4806299e7f4913f5d0473abec63cb8a5
yongjiang-xiao <2477865846@qq.com>
2026-07-20 14:45:03 +0800

    feat: 实现 Neo4j 文档结构化存储

--- graphdb/__init__.py
+++ graphdb/__init__.py
@@ -1,14 +1,24 @@
 """Neo4j + Chroma database and retrieval module."""
 
 from graphdb.config import GraphDBConfig
+from graphdb.database_repository import (
+    ChildChunkNode,
+    DatabaseRepository,
+    DocumentNode,
+    ParentChunkNode,
+)
 from graphdb.embedding import HashEmbeddingFunction, create_embedding_function
 from graphdb.neo4j_client import Neo4jClient
 from graphdb.chroma_client import ChromaClient
 
 __all__ = [
+    "ChildChunkNode",
     "ChromaClient",
+    "DatabaseRepository",
+    "DocumentNode",
     "GraphDBConfig",
     "HashEmbeddingFunction",
     "Neo4jClient",
+    "ParentChunkNode",
     "create_embedding_function",
 ]
